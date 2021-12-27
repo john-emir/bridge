9b7cc9e0f890c33f7028e16dbc2aa57fdef5b4f7
john-emir <62830033+john-emir@users.noreply.github.com>
2021-12-27 18:00:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// let url = "https://john-emir.github.io/B.POIT/"
+// function loadBodyPage(taskId){
+//     fetch(url)
+//     .then(function(response){
+//         return response.text();
+//     }).then(function(data){
+
+//         document.body.insertAdjacentHTML('beforeend', data);
+        
+//     }).catch(function(error){
+//         alert(error);
+//     })
+
+// }
